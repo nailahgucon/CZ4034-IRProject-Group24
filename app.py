e915dd6fe2fadc67c7f6cc8fdad0c7184378aafd
Liang Zhiheng <liangzhiheng62@gmail.com>
2023-03-23 23:43:16 +0800

    updated the ui

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import json
+import math
 from flask import Flask, render_template, request
 from datetime import datetime
+import records
 import requests
 
 from spellcheck import spellcheck
@@ -13,8 +15,8 @@ app = Flask(__name__)
 
 server:str = "http://localhost:8983/solr/reviews/select"
 col_name:str = "spellCheck"  # or any other copyfields
-query_term:str = "Sparrow"
-mlt_field = "Type"
+query_term:str = "Review"
+mlt_field = "Rating"
 kwargs = {'spellcheck.build': "true",
           'spellcheck.reload': "true",
           'spellcheck': 'true',
@@ -28,147 +30,159 @@ kwargs = {'spellcheck.build': "true",
 
 mlt_url = "http://localhost:8983/solr/reviews/mlt"
 
-class records():
-    docs = None
-    @classmethod
-    def store(cls, docs):
-      print("document ", docs[0]["Title"][0])
-      cls.docs = docs
-    
-    def filterByRating(self, rating):
-      result = []
-      for i in self.docs:
-        if i["Rating"] == rating:
-          result.append(i)
-      return result
-    
-    def filterByDate(self, start_date, end_date):
-      result = []
-      for i in self.docs:
-        date = datetime.strptime(i["DateOfReview"][0], '%B %d, %Y')
-        
-        if date >= start_date and date <= end_date:
-          result.append(i)
-      return result
-    
-    def filterByDateAndRating(self,start_date, end_date,rating):
-      result = []
-      for i in self.docs:
-        date = datetime.strptime(i["DateOfReview"][0], '%B %d, %Y')
-        if date >= start_date and date <= end_date and i["Rating"] == rating:
-          result.append(i)
-      return result
-    
-    def getAllRecords(self):
-      return self.docs
-
 @app.route('/', methods=['GET'])
-def index():
+def home():
 
-  # Build Solr query
-  query = {
-      "q": "*:*",
-      "fq": "*:*",
-      "rows": 100000,
-      "start": 0,
-      "mlt":"true",
-      "spellcheck.build": "true",
-      "spellcheck.reload": "true",
-      "spellcheck": "true",
-      "wt": "json"
-  }
-  URL = "http://localhost:8983/solr/reviews/select"
+  # # Build Solr query
+  # query = {
+  #     "q": "*:*",
+  #     "fq": "*:*",
+  #     "rows": 100000,
+  #     "start": 0,
+  #     "mlt":"true",
+  #     "spellcheck.build": "true",
+  #     "spellcheck.reload": "true",
+  #     "spellcheck": "true",
+  #     "wt": "json"
+  # }
+  # URL = "http://localhost:8983/solr/reviews/select"
   
-  # Send Solr query
-  response = requests.get(URL, params=query)
+  # # Send Solr query
+  # response = requests.get(URL, params=query)
   
-  # Parse Solr results
-  results = json.loads(response.text)["response"]["docs"]
+  # # Parse Solr results
+  # results = json.loads(response.text)["response"]["docs"]
 
-  myRcords = records()
-  myRcords.store(results)
+  # # myRcords = records.records()
+  # # myRcords.store(results)
+  
+  return render_template('home.html')
 
-  # return "nice" 
-  render_template('index.html', docs=results)
-
-@app.route('/query', methods=['GET', 'POST'])
+@app.route('/query', methods=['GET'])
 def query():
   if request.method == 'GET':
-    # queryContent = request.values.get("content").replace(" ","%20")
-    # filed = "Review"
-    user_query = f"{col_name}:{query_term}"
-    kwargs.update({"q": user_query})
-
-    '''
-    example of kwargs(additional params on top of query):
-    {'spellcheck.build': "true",
-     'spellcheck.reload': "true",
-     'spellcheck': "true"}
-    '''
-    response = requests.get(server,
-                           params=kwargs)
-
+    query_term = request.values.get("content").replace(" ","%20")
+    filed = "Review"
+    # Build Solr query
+    query = {
+        "q": "*:*",
+        "fq": f"{filed}:{query_term}",
+        "rows": 10000,
+        "start": 0,
+        "spellcheck.build": "true",
+        "spellcheck.reload": "true",
+        "spellcheck": "true",
+        "wt": "json",
+    }
+    URL = "http://localhost:8983/solr/reviews/select"
     
-    results = json.loads(response.text)
+    # Send Solr query
+    response = requests.get(URL, params=query)
     
-    # check results for typos
-    spellcheck_list = results.get("spellcheck").get("collations")
-    if spellcheck_list:
-        correct_terms, typo_terms, collation_queries = spellcheck(spellcheck_list)
-        print("Are these what you mean?")
-        # if more than 1 correct terms
-        for terms in collation_queries:
-          term_ = terms.split('\n')
-          # takes the odd indices
-          term = ' '.join([
-            term_[i] for i in range(len(term_))
-            if i % 2 == 1])
-          print(term)
-          # # can re-query
-          # res2 = query(col_name, correct_term, server, **kwargs)
-          # res.update(res2)
-    # else:
-    #     typo_res.update(results)
-    
-    # check results for suggestions
-    suggtexts = autocomplete(results)
-    print(f"Your query is {query_term}, are you looking for {suggtexts}?")
-
-    # more like this
-    out = mlt(results)
-    print(f"output for more like this is {out}")
-
     # Parse Solr results
     results = json.loads(response.text)["response"]["docs"]
+    # user_query = f"{col_name}:{query_term}"
+    # kwargs.update({"q": user_query})
 
-    myRcords = records()
-    myRcords.store(results)
-    return render_template('index.html', docs=results)
+    # '''
+    # example of kwargs(additional params on top of query):
+    # {'spellcheck.build': "true",
+    #  'spellcheck.reload': "true",
+    #  'spellcheck': "true"}
+    # '''
+    # response = requests.get(server,
+    #                        params=kwargs)
+
+    
+    # results = json.loads(response.text)
+    
+    # print(results)
+
+    # # check results for typos
+    # spellcheck_list = results.get("spellcheck").get("collations")
+    # if spellcheck_list:
+    #     correct_terms, typo_terms, collation_queries = spellcheck(spellcheck_list)
+    #     print("Are these what you mean?")
+    #     # if more than 1 correct terms
+    #     for terms in collation_queries:
+    #       term_ = terms.split('\n')
+    #       # takes the odd indices
+    #       term = ' '.join([
+    #         term_[i] for i in range(len(term_))
+    #         if i % 2 == 1])
+    #       print(term)
+    #       # # can re-query
+    #       # res2 = query(col_name, correct_term, server, **kwargs)
+    #       # res.update(res2)
+    # # else:
+    # #     typo_res.update(results)
+    
+    # # check results for suggestions
+    # suggtexts = autocomplete(results)
+    # print(f"Your query is {query_term}, are you looking for {suggtexts}?")
+
+    # # more like this
+    # out = mlt(results)
+    # print(f"output for more like this is {out}")
+
+    # # Parse Solr results
+    # results = json.loads(response.text)["response"]["docs"]
+
+
+    # save the results to records class
+    myRecords = records.records()
+    myRecords.store(results)
+    
+    # for pagination, only display 10 records
+    totalPages = int(math.ceil(len(results) / 10))
+    displayResult = results[0:10]
+
+    return render_template('results.html', docs=displayResult, current_page=1, total_pages=totalPages)
   else:
-    return render_template('index.html')
-  
+    return render_template('home.html')
+
+@app.route('/pagination')
+def pagination():
+    myRecords = records.records()
+    results = myRecords.getDisplayRecords()
+    totalPages = int(math.ceil(len(results) / 10))
+    page = int(request.values.get("page"))
+    if page < 1:
+        page = 1
+    start = page * 10 - 10
+    end = 0 + page * 10
+    if len(results) < end:
+        displayResult = results[start : len(results)]
+    else:
+        displayResult = results[start:end]
+    print("length of the results ", len(results))
+    return render_template('results.html', docs=displayResult, current_page=page, total_pages=totalPages)
+
 
 @app.route('/filter', methods=['GET'])
 def filter():
   if request.method == 'GET':
 
-    myRecords = records()
+    myRecords = records.records()
+
+    # type cast the rating filter
     rating_filter = request.values.get('rating')
     formatted_rating = None
     if rating_filter:
       formatted_rating = int(rating_filter[0])
 
+    # type cast the date filter
     start_date_filter = request.values.get('startDate')
     end_date_filter = request.values.get('endDate')
     formatted_start_date = None
     formatted_end_date = None
-
     if start_date_filter and end_date_filter:
       start_date_obj = datetime.strptime(start_date_filter, '%Y-%m-%d')
       end_date_obj = datetime.strptime(end_date_filter, '%Y-%m-%d')
       formatted_start_date = datetime.strptime(start_date_obj.strftime('%B %d, %Y'), '%B %d, %Y')
       formatted_end_date = datetime.strptime(end_date_obj.strftime('%B %d, %Y'), '%B %d, %Y')
 
+    # actual filter
     if formatted_rating and formatted_start_date and formatted_end_date:
       result = myRecords.filterByDateAndRating(formatted_start_date, formatted_end_date, formatted_rating)
     elif formatted_rating:
@@ -178,9 +192,16 @@ def filter():
     else:
       result = myRecords.getAllRecords() 
     
-    return render_template('index.html', docs = result)
+    totalPages = int(math.ceil(len(result) / 10))
+    displayResult = result[0:10]
+
+    return render_template('results.html', docs = displayResult, current_page=1, total_pages=totalPages)
   else:
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route("/crawling", methods=['GET'])
+def crawling():
+    return render_template("crawling.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ records.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+
+class records():
+    docs = None
+    displayDocs = []
+
+    @classmethod
+    def store(cls, docs):
+      print("document ", docs[0]["Title"][0])
+      cls.displayDocs = docs
+      cls.docs = docs
+      print("save records to displatDocs and docs")
+    
+    def filterByRating(self, rating):
+      result = []
+      for i in self.docs:
+        if i["Rating"] == rating:
+          result.append(i)
+
+      self.displayDocs = result
+      return self.displayDocs
+    
+    def filterByDate(self, start_date, end_date):
+      result = []
+      for i in self.docs:
+        date = datetime.strptime(i["DateOfReview"][0], '%B %d, %Y')
+        
+        if date >= start_date and date <= end_date:
+          result.append(i)
+      
+      self.displayDocs = result
+      return self.displayDocs
+    
+    def filterByDateAndRating(self,start_date, end_date,rating):
+      result = []
+      for i in self.docs:
+        date = datetime.strptime(i["DateOfReview"][0], '%B %d, %Y')
+        if date >= start_date and date <= end_date and i["Rating"] == rating:
+          result.append(i)
+      
+      self.displayDocs.append(i)
+      return self.displayDocs
+    
+    def getAllRecords(self):
+      return self.docs
+    
+    def getDisplayRecords(self):
+      return self.displayDocs
