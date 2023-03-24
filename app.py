39ac34758018b159ade7bc094bfb79918fd40eeb
Liang Zhiheng <liangzhiheng62@gmail.com>
2023-03-24 23:40:25 +0800

    update ui and pagination

--- app.py
+++ app.py
@@ -58,6 +58,36 @@ def home():
   
   return render_template('home.html')
 
+@app.route('/getAllRecords', methods=['GET'])
+def getAllrecords():
+  # Build Solr query
+  query = {
+      "q": "*:*",
+      "rows": 10000,
+      "start": 0,
+      "spellcheck.build": "true",
+      "spellcheck.reload": "true",
+      "spellcheck": "true",
+      "wt": "json",
+  }
+  URL = "http://localhost:8983/solr/reviews/select"
+  
+  # Send Solr query
+  response = requests.get(URL, params=query)
+  
+  # Parse Solr results
+  results = json.loads(response.text)["response"]["docs"]
+
+  # save the results to records class, don't change myRecords and totalPages.
+  myRecords = records.records()
+  myRecords.store(results)
+  
+  # for pagination, only display 10 records
+  totalPages = int(math.ceil(len(results) / 10))
+  displayResult = results[0:10]
+  
+  return render_template('results.html', docs=displayResult, current_page=1, total_pages=totalPages)
+
 @app.route('/query', methods=['GET'])
 def query():
   if request.method == 'GET':
