f67219c84b564a98f0e90d8254e24f7cbc31775d
Liang Zhiheng <liangzhiheng62@gmail.com>
2023-03-29 11:30:55 +0800

    add API for delete documents

--- app.py
+++ app.py
@@ -244,6 +244,23 @@ def crawling():
     subprocess.call(['python', os.getcwd() + '\\crawling\\crawl_hotels_links.py'])
     subprocess.call(['python', os.getcwd() + '\\crawling\\crawling_eateries.py'])
     subprocess.call(['python', os.getcwd() + '\\crawling\\crawling_hotels.py'])
+
+    # delete documents
+    # import xml.etree.ElementTree as ET
+
+    # URL = "http://localhost:8983/api/collections/reviews/update?commit=true"
+
+    # header = {"Content-Type": "text/xml"}
+
+    # root = ET.Element("delete")
+    # query = ET.SubElement(root, "query")
+    # query.text = "*:*"
+    # xml_payload = ET.tostring(root)
+
+    # # print(xml_payload)
+    # # Send the HTTP request to Solr
+    # response = requests.get(URL, headers=header, data=xml_payload)
+
     return render_template("crawling.html")
 
 if __name__ == "__main__":
