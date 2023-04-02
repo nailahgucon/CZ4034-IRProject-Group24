a4999100731171835ae02d2134074de628459acb
Kam Chin Voon <57339291+kcvvck@users.noreply.github.com>
2023-04-02 23:29:40 +0800

    added config file

--- config/config.py
+++ config/config.py
@@ -27,3 +27,18 @@ all_data_header = ["Name","Category","Style",
                    "Star","lat","lon"]
 
 API_KEY = 'AIzaSyCEl2l5qFEY-Qf1_VjelMlooH5PjTgOdhM'  # your API key here
+
+
+NEAR_WORDS = ["near", "nearer", "close", "around"]
+
+CAT_EATERY = ['eateries', 'eatery', 'eat']
+CAT_HOTEL = ['hotel', 'hotels', 'stay']
+
+FILTER_WORDS_TOP = ["best", "top", "popular", "high", "highest"]
+FILTER_WORDS_BOT = ["worst", "worse", "least", "bad", "bottom"]
+
+WORDS = NEAR_WORDS+CAT_EATERY+CAT_HOTEL+FILTER_WORDS_TOP+FILTER_WORDS_BOT
+
+
+defaultURL = "http://localhost:8983/solr/reviews/select?q=%s&facet.field={!key=distinctStyle}distinctStyle&facet=on&rows=10000&wt=json&json.facet={distinctStyle:{type:terms,field:distinctStyle,limit:10000,missing:false,sort:{index:asc},facet:{}}}"
+
